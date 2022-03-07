a9d65fb1c29f519d9c1eaa969a80677e6ac3f619
VuTrieu01 <vutrieu815@gmail.com>
2022-03-07 12:11:30 +0700

    page contest

--- src/routes/ClientRoutes.jsx
+++ src/routes/ClientRoutes.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Navigate, Route, Routes as Switch } from 'react-router-dom';
+
+import Contest from '../features/contestPage/Contest';
+
 export function ClientRoutes() {
   return (
     <Switch>
       <Route path="/" element={<Navigate to="/problem" />} />
       <Route path="/problem" element={<div>Problem page</div>} />
-      <Route path="/contest" element={<div>Contest page</div>} />
+      <Route
+        path="/contest"
+        element={
+          <div>
+            <Contest />
+          </div>
+        }
+      />
       <Route path="/rank" element={<div>Rank page</div>} />
       <Route path="/upload" element={<div>upload page</div>} />
     </Switch>
