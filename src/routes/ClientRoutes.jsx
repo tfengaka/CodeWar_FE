c927125f4c417c86b21ab672e802a07fae8ef042
buivantan01 <buivantan1234566@gmail.com>
2022-03-07 16:31:16 +0700

    First commit problem page layout

--- src/routes/ClientRoutes.jsx
+++ src/routes/ClientRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navigate, Route, Routes as Switch } from 'react-router-dom';
+import Problem from '../feature/pages/problem/Problem';
 export function ClientRoutes() {
   return (
     <Switch>
       <Route path="/" element={<Navigate to="/problem" />} />
-      <Route path="/problem" element={<div>Problem page</div>} />
+      <Route path="/problem" element={<Problem />} />
       <Route path="/contest" element={<div>Contest page</div>} />
       <Route path="/rank" element={<div>Rank page</div>} />
       <Route path="/upload" element={<div>upload page</div>} />
