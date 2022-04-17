f89d4beddfe3dad67807046337d557c49decce3c
MSI\Admin <hoanghaiick28@gmail.com>
2022-04-17 12:17:18 +0700

    update features/discuss

--- src/features/discuss/components/Comment.jsx
+++ src/features/discuss/components/Comment.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Comment() {
   return (
-    <div>Comment kahbfkahsfbdkahs</div>
+    <div className='discuss'>
+      <div>
+        <div className='promo-banner'>
+          <img className='img-progess' src="https://images.viblo.asia/full/ab87f1cf-dab2-417e-b47b-c97afe3d82d7.png" alt="" />
+        </div>
+        <div className='text-center'>
+          <a  href="#">
+          &gt;&gt; Tham gia Facebook group "Viblo Community" để cùng nhau học tập và kết nối &lt;&lt;
+           </a>
+        </div>
+        <div className='container-comment'> 
+          <h3>
+            <strong>THẢO LUẬN</strong>
+          </h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
