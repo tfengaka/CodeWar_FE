f1459e7d366c982204787cd3e795e04e617da786
VuTrieu01 <vutrieu815@gmail.com>
2022-05-10 21:44:06 +0700

    create contest

--- src/features/contest/admin/pages/CreateContest.jsx
+++ src/features/contest/admin/pages/CreateContest.jsx
@@ -6,36 +6,42 @@ import 'react-datepicker/dist/react-datepicker.css';
 const CreateContest = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [questionList, setQuestionList] = useState([{ question: '' }]);
+
+  const handleQuestionAdd = () => {
+    setQuestionList([...questionList, { question: '' }]);
+  };
+
+  const handleQuestionRemove = (index) => {
+    const list = [...questionList];
+    list.splice(index, 1);
+    setQuestionList(list);
+  };
   return (
     <>
-      <div className='create_container'>
-        <div className='create_description'>
-          <ul className='create_card'>
+      <div className="create_container">
+        <div className="create_description">
+          <ul className="create_card">
             <li>
               <h3>Tiêu đề</h3>
-              <div className='create_card--input'>
-                <input
-                  autoComplete='off'
-                  spellCheck='false'
-                  type='text'
-                  placeholder='Tiêu đề'
-                ></input>
+              <div className="create_card--input">
+                <input autoComplete="off" spellCheck="false" type="text" placeholder="Tiêu đề"></input>
               </div>
             </li>
             <li>
               <h3>Nội dung</h3>
-              <div className='create_card--input'>
-                <textarea autoComplete='off' spellCheck='false' placeholder='Nội dung'></textarea>
+              <div className="create_card--input">
+                <textarea autoComplete="off" spellCheck="false" placeholder="Nội dung"></textarea>
               </div>
             </li>
             <li>
               <h3>Ngày giờ bắt đầu</h3>
-              <div className='create_card--input'>
+              <div className="create_card--input">
                 <DatePicker
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
-                  timeInputLabel='Time:'
-                  dateFormat='dd/MM/yyyy h:mm aa'
+                  timeInputLabel="Time:"
+                  dateFormat="dd/MM/yyyy h:mm aa"
                   showTimeInput
                   minDate={new Date()}
                 />
@@ -43,23 +49,37 @@ const CreateContest = () => {
             </li>
             <li>
               <h3>Ngày giờ kết thúc</h3>
-              <div className='create_card--input'>
+              <div className="create_card--input">
                 <DatePicker
                   selected={endDate}
                   onChange={(date) => setEndDate(date)}
-                  timeInputLabel='Time:'
-                  dateFormat='dd/MM/yyyy h:mm aa'
+                  timeInputLabel="Time:"
+                  dateFormat="dd/MM/yyyy h:mm aa"
                   showTimeInput
                   minDate={startDate}
                 />
               </div>
             </li>
-            <li>
-              <h3>Câu hỏi</h3>
-              <div className='create_card--input'>
-                <textarea autoComplete='off' spellCheck='false' placeholder='Câu hỏi'></textarea>
-              </div>
-            </li>
+            {questionList.map((item, index) => (
+              <li>
+                <h3>Câu hỏi</h3>
+                <div key={index} className="create_card--input">
+                  <textarea autoComplete="off" spellCheck="false" placeholder="Câu hỏi"></textarea>
+                </div>
+                <div className="create_card--button">
+                  {questionList.length - 1 === index && questionList.length < 4 && (
+                    <button type="button" className="btnAdd" onClick={handleQuestionAdd}>
+                      Thêm câu hỏi
+                    </button>
+                  )}
+                  {questionList.length > 1 && (
+                    <button type="button" className="btnRemove" onClick={() => handleQuestionRemove(index)}>
+                      Xóa
+                    </button>
+                  )}
+                </div>
+              </li>
+            ))}
             <Button>Lưu</Button>
           </ul>
         </div>
