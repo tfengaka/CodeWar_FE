dd31a6e4bbdda7b3639322bebb68863801c7ba8d
Nguyễn Thanh Hòa <nthoa2.lhp.nh@gmail.com>
2022-05-12 23:40:01 +0700

    merge contest

--- src/components/DropdownCollapse.jsx
+++ src/components/DropdownCollapse.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import useCollapse from 'react-collapsed';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const DropdownCollapse = ({ title, children }) => {
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+  const { pathname } = useLocation();
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({ duration: 350 });
   return (
     <>
       <div className={`option_button ${isExpanded ? 'active' : ''}`} {...getToggleProps()}>
         {title}
-        <i className="bx bx-list-ul"></i>
+        <i className={`bx bx-chevron-down ${isExpanded ? 'show' : ''}`}></i>
       </div>
       <div className="option_children" {...getCollapseProps()}>
         {children.map((item, itemIndex) => (
           <div key={itemIndex} className="option_children_item">
-            <Link to={item.path} className="option_children_item_link">
+            <Link to={item.path} className={`option_children_item_link ${pathname === item.path ? 'active' : ''}`}>
               {item.name}
             </Link>
           </div>
