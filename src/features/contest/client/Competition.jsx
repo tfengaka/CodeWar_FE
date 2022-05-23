6f030a53948c39023304fc99721a491fb3eafd03
vodoanhoanglong <vodoanhoanglong10a9@gmail.com>
2022-05-23 18:58:59 +0700

    Update competition

--- src/features/contest/client/Competition.jsx
+++ src/features/contest/client/Competition.jsx
@@ -1,11 +1,24 @@
+import Button from 'components/Button';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import ProblemSolve from '../../../components/ProblemSolve';
 
 const Competition = () => {
   const location = useLocation();
   let { data } = location.state;
-  console.log(data);
-  return <div>Competition</div>;
+  return (
+    <div>
+      <div>
+        {data.map((item, index) => (
+          <Button>{index + 1}</Button>
+        ))}
+      </div>
+      <div>
+        <ProblemSolve />
+      </div>
+      <div>bottom</div>
+    </div>
+  );
 };
 
 export default Competition;
